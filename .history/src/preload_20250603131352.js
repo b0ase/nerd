664a24c5f3b32e2a296e5d58c53646bbc2d11cbd
b0ase <your-email@example.com>
2025-06-03 16:26:39 +0100

    🧹 Clean up repository - Remove IDE history files and update .gitignore

--- .history/src/preload_20250603131352.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  selectFiles: () => ipcRenderer.invoke('select-files'),
-  renameFiles: (filePaths) => ipcRenderer.invoke('rename-files', filePaths)
-}); 
--- .history/src/preload_20250603132251.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  selectFiles: () => ipcRenderer.invoke('select-files'),
-  selectOutputFolder: () => ipcRenderer.invoke('select-output-folder'),
-  processFiles: (filePaths, outputFolder) => ipcRenderer.invoke('process-files', filePaths, outputFolder)
-}); 
